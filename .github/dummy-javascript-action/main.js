8ced062865a4bd1fba5b36b2694ce6232719b014
William Barros Costa <125475866+william-barros-costa@users.noreply.github.com>
2024-09-01 04:02:56 -0700

    Update main.js

--- .github/dummy-javascript-action/main.js
+++ .github/dummy-javascript-action/main.js
@@ -4,3 +4,13 @@ const github = require('@actions/github')
 const exec = require('@actions/exec')
 
 core.notice('This is the main.js file')
+
+// Get input
+const input_name = core.input('sample_input', {required: true})
+core.notice(`Sample Input is: ${input_name}`)
+
+// Run bash command
+exec.exec("echo 'This is an exec command'")
+
+// Get github action name
+core.notice(github.context.action)
