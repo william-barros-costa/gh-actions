27a846b60ea60f81ee81c4d04dc56a0662831837
William Barros Costa <125475866+william-barros-costa@users.noreply.github.com>
2024-09-01 03:35:23 -0700

    Update main.js

--- .github/deploy-s3-javascript/main.js
+++ .github/deploy-s3-javascript/main.js
@@ -1,1 +1,6 @@
 // For this to run with success, locally, we will need to install @actions/core @actions/github @actions/exec. They are present in the actions/toolkit from github actions
+const core = require('@actions/core')
+const github = require('@actions/github')
+const exec = require('@actions/exec')
+
+core.notice('This is the main.js file')
